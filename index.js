6e55302ee5ba169ebc0dd8bd8fd4f1cb2a0c218a
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-19 19:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,18 @@ function nowServing(currentLine){
   }
 }
   
-
+function currentLine(lineOfPeople){
+  returnString = "The line is currently: "
+  if (lineOfPeople.length === 0){
+    
+    return "The line is currently empty."
+  } else {
+    for (var i =0; i < lineOfPeople.length; i++){
+      returnString += (i + 1) + ". " + lineOfPeople[i] + " "
+    }
+  }
+  
+  return returnString
+    
+}//currentLine
   
