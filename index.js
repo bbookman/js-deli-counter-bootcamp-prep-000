5916e05ccabb083469009b38c184635dc161c99c
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-19 19:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ function nowServing(currentLine){
     return  "Currently serving " + currentLine.shift() +".";
     
   }
+}
   
 function currentLine(lineOfPeople){
   returnString = "The line is currently: "
   if (lineOfPeople.length === 0){
     
     return "The line is currently empty."
-  } else {
-    for (var i =0; i < lineOfPeople.length; i++){
+    
+   } else {
+      for (var i =0; i < lineOfPeople.length; i++){
       returnString += (i + 1) + ". " + lineOfPeople[i] + " "
     }
   }
   
   return returnString
     
-}//currentLine
-
-  
 }
+  
