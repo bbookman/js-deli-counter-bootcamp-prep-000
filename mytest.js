575f8d4b9d6aa6b208f1c9210b9119b61a561be8
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-19 19:15:48 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -1,19 +1,7 @@
 line = ["Betty","Wilma"]
 
 function currentLine(lineOfPeople){
-    var returnString = "The line is currently ";
     
-    if lineOfPeople.length == 0 {
-      return "The line is currently empty."
-    } else {
-      for (var i = 0; i < lineOfPeople.length; i++){
-        if (lineOfPeople.length == 1){
-          returnString += (i + 1) + "." + lineOfPeople[i] + " is the last person in line"
-        }
-        return returnString
-    }
- 
-    
-  }
+}//currentLine
 
 console.log(currentLine(line));
